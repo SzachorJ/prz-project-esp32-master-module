317668d5e64f7269933b3023bc3301bf9a557a04
Sebafire1 <deteriop1676@gmail.com>
2022-12-11 23:27:20 +0100

    added function which combine string output

--- prz-project-esp32-master-module/src/main.cpp
+++ prz-project-esp32-master-module/src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 
-void setup() {
+String analogReadCombine(String module1, String module2, String module3)
+{
+  String combine = module1;
+  String combine += module2;
+  String combine += module3;
+  return combine;
+}
+void setup()
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
